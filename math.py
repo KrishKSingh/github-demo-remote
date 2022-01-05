2dfbfe26cd7655d086b98b30567aa6683b59deab
krishnasingh <krishna_singh3@persistent.com>
2022-01-05 22:40:21 +0530

    Added fn headers

--- math.py
+++ math.py
@@ -1,22 +1,22 @@
-# Add implementation
+# Add implementation method
 def add(x,y):
     return x + y
 
-# Subtract implementation
+# Subtract implementation method
 def subtract(x,y):
     return x - y    #on master branch
 
-# Multiply implementation
+# Multiply implementation method
 def multiply(x,y):
     return x * y    # on Bug456 branch
 
-# Divide implementation
+# Divide implementation method
 def divide(x,y):
     if y == 0:      # on master branch	
         return DIVIDE_BY_0_ERROR
     else:
         return x / y
         
-# Square Implentation        
+# Square Implentation method
 def square(x):
     return x * x
